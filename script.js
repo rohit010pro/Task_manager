4c2c9e64f4ff602a12cadc759f20d08078cec588
Rohit01-pro <rohit777kanojiya@gmail.com>
2022-05-07 16:44:46 +0530

    Filter menu added

--- script.js
+++ script.js
@@ -1,74 +1,106 @@
-const inputTask = document.querySelector('#input_task'),
-tasklist = document.querySelector('.task_list'),
-addFormBtn = document.querySelector('#showAddBtn'),
-addForm = document.querySelector('.add_task');
+const 
+    addFormBtn = document.querySelector("#show-add-form-btn"),
+    inputTask = document.querySelector("#input_task"),
+    tasklist = document.querySelector(".task_list"),
+    addForm = document.querySelector(".add_task"),
+    filters = document.querySelectorAll(".filters span"),
+    clearAllBtn = document.querySelector('.clear-btn');
 
 var isEdit = false;
 var editId = 0;
 
 // Display Task
-const showTasks = () => {
-    const allTask = JSON.parse(localStorage.getItem('tasks'));
+const showTasks = (filter = "all") => {
+    const allTask = JSON.parse(localStorage.getItem("tasks"));
+    let html = "";
 
-    let html = `<h3>Task List</h3>`;
-
-    if (allTask === null) {
-        html += `<div class="empty">No task to show</div>`;
-    } else {
-        for (let i = 0; i < allTask.length; i++) {
-            html +=
-                `
-            <div class="task">
-                <h4>${allTask[i].taskTitle}</h4>
-                <button class="edit" onclick="editTask(${allTask[i].id},'${allTask[i].taskTitle}')">
-                    <span class="material-symbols-outlined">edit</span>
-                </button>
-                <button class="delete" onclick="deleteTask(${allTask[i].id})">
-                    <span class="material-symbols-outlined">delete</span>
-                </button>
-            </div>`;
-        }
+    if (allTask !== null) {
+        allTask.forEach((task)=> {
+            if(filter == task.status || filter == "all"){
+                let completed = (task.status == "completed") ? "checked" : "";
+                html += `
+                <div class="task">
+                    <label for="${task.id}">
+                        <input onclick="updateStatus(this)" type="checkbox" id="${task.id}" ${completed}>
+                        <p class="task_name ${completed}">${task.taskTitle}</p>
+                    </label>
+                    <button class="edit" onclick="editTask(${task.id},'${task.taskTitle}')">
+                        <span class="material-symbols-outlined">edit</span>
+                    </button>
+                    <button class="delete" onclick="deleteTask(${task.id})">
+                        <span class="material-symbols-outlined">delete</span>
+                    </button>
+                </div>`;
+            }
+        });
     }
-    tasklist.innerHTML = html;
-}
+    tasklist.innerHTML = html ||  `<div class="empty">No task to show</div>`;
+
+    let checkTask = tasklist.querySelectorAll(".task");
+    !checkTask.length ? clearAllBtn.classList.remove("active") : clearAllBtn.classList.add("active");
+};
 
 // Adding Task
 const addTask = (e) => {
-    if(e.key != "Enter")
-        return;
-    let taskTitle = htmlEntities(inputTask.value.trim());
+    if (e.key != "Enter") return;
 
-    let parent = inputTask.parentElement;
+    let taskTitle = htmlEntities(inputTask.value.trim()),
+    parent = inputTask.parentElement;
 
     if (taskTitle === "") {
-        parent.classList.add('error');
+        parent.classList.add("error");
         return;
-    } else
-        parent.classList.remove('error');
+    } else 
+        parent.classList.remove("error");
 
-    const allTask = JSON.parse(localStorage.getItem('tasks'));
+    const allTask = JSON.parse(localStorage.getItem("tasks"));
     let updatedTask;
 
-    if(isEdit){
+    if (isEdit) {
         for (let i = 0; i < allTask.length; i++) {
-            if(allTask[i].id == editId)
+            if (allTask[i].id == editId){
                 allTask[i].taskTitle = taskTitle;
+                break;
+            }
         }
         isEdit = false;
         updatedTask = allTask;
-    }else{
+    } else {
         let id = allTask === null ? 1 : allTask.length + 1;
 
-        let newtask = { id, taskTitle };
-        if (allTask === null)
-            updatedTask = [newtask];
-        else
-            updatedTask = [...allTask, newtask];
+        let newtask = {
+            id,
+            taskTitle,
+            status: 'pending'
+        };
+        updatedTask = (allTask === null) ? [newtask] : [...allTask, newtask];
     }
 
     localStorage.setItem("tasks", JSON.stringify(updatedTask));
 
-    inputTask.value = '';
+    inputTask.value = "";
+    showTasks();
+};
+
+const updateStatus = (selectedTask) =>  {
+    const allTask = JSON.parse(localStorage.getItem("tasks"));
+
+    let task = selectedTask.parentElement.querySelector('.task_name');
+
+    if(selectedTask.checked){
+        task.classList.add("checked");
+        allTask.forEach((task)=>{
+            if(selectedTask.id == task.id)
+                task.status = 'completed';
+        });
+    }else{
+        task.classList.remove("checked");
+        allTask.forEach((task)=>{
+            if(selectedTask.id == task.id)
+                task.status = 'pending';
+        });
+    }
+    localStorage.setItem("tasks", JSON.stringify(allTask));
     showTasks();
 }
 
@@ -77,38 +109,61 @@ const editTask = (id, taskTitle) => {
     inputTask.value = taskTitle;
     isEdit = true;
     editId = id;
-    addForm.classList.add('show');
-    addFormBtn.classList.add('rotate');
-}
+    addForm.classList.add("show");
+    addFormBtn.classList.add("rotate");
+};
 
 // Deleting Task
 const deleteTask = (id) => {
-    let tasks = JSON.parse(localStorage.getItem("tasks"));
-    
-    let result = tasks.filter((task)=>{
-        if(task.id !== id)
-            return task;
+    let allTask = JSON.parse(localStorage.getItem("tasks"));
+
+    if(allTask === null) {
+        showTasks();
+        return;
+    }
+
+    let result = allTask.filter((task) => {
+        if (task.id !== id) return task;
     });
 
-    if(result.length == 0) result = null;
+    if (result.length == 0) result = null;
 
     localStorage.setItem("tasks", JSON.stringify(result));
     showTasks();
-}
+};
 
-function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
-
-addFormBtn.addEventListener('click', () => {
-    addForm.classList.toggle('show');
-    addFormBtn.classList.toggle('rotate');
+clearAllBtn.addEventListener("click", () => {
+    let allTask = JSON.parse(localStorage.getItem("tasks"));
+    allTask = null;
+    localStorage.setItem("tasks", JSON.stringify(allTask));
+    showTasks();
 });
 
-inputTask.addEventListener('input', () => {
+const htmlEntities = (str) => {
+    return String(str)
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;");
+};
+
+addFormBtn.addEventListener("click", () => {
+    addForm.classList.toggle("show");
+    addFormBtn.classList.toggle("rotate");
+});
+
+inputTask.addEventListener("input", () => {
     let parent = inputTask.parentElement;
-    parent.classList.remove('error');
+    parent.classList.remove("error");
+});
+
+filters.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        document.querySelector("span.active").classList.remove("active");
+        btn.classList.add("active");
+        showTasks(btn.id);
+    });
 });
 
 showTasks();
-inputTask.addEventListener('keyup', addTask);
+inputTask.addEventListener("keyup", addTask);
